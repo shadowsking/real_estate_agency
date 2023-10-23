ae7ffbaf8ac9af59f90c29f3d99641b9177d5de7
shadowsking <shadowsking1295@gmail.com>
2023-10-23 04:23:53 +0300

    Added db model for complaints. Moved admin models.

--- property/admin.py
+++ property/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 
-from .models import Flat, FlatAdmin
+from .models import Flat, Complaint
 
 
-admin.site.register(Flat, FlatAdmin)
+@admin.register(Flat)
+class FlatAdmin(admin.ModelAdmin):
+    search_fields = ('town', 'address', 'owner',)
+    readonly_fields = ('created_at',)
+    list_display = ('address', 'price', 'new_building', 'construction_year', 'town',)
+    list_editable = ('new_building',)
+    list_filter = ('new_building', 'rooms_number', 'has_balcony',)
+
+
+@admin.register(Complaint)
+class ComplaintAdmin(admin.ModelAdmin):
+    list_display = ('author', 'flat',)
+    raw_id_fields = ('author', 'flat',)
--- property/models.py
+++ property/models.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
@@ -61,9 +61,22 @@ class Flat(models.Model):
         return f'{self.town}, {self.address} ({self.price}р.)'
 
 
-class FlatAdmin(admin.ModelAdmin):
-    search_fields = ('town', 'address', 'owner',)
-    readonly_fields = ('created_at',)
-    list_display = ('address', 'price', 'new_building', 'construction_year', 'town',)
-    list_editable = ('new_building',)
-    list_filter = ('new_building', 'rooms_number', 'has_balcony',)
+class Complaint(models.Model):
+    author = models.ForeignKey(
+        User,
+        verbose_name='Кто жаловался',
+        related_name='complaints',
+        on_delete=models.CASCADE
+    )
+    flat = models.ForeignKey(
+        Flat,
+        verbose_name='Квартира, на которую пожаловались',
+        related_name='complaints',
+        on_delete=models.CASCADE
+    )
+    text = models.TextField(
+        verbose_name='Текст жалобы'
+    )
+
+    def __str__(self):
+        return f'Автор жалобы: {self.author.username}, {self.flat.address}'
